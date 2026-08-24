5d438eba994046b1a972725bc84328232f8a41bb
Darsh <darsh@example.com>
2026-08-24 09:47:23 +0530

    Add variables practice exercises

---
+++ 02.Variables/1.py
@@ -0,0 +1,44 @@
+name="Rahul"
+age=18
+city="Junagadh" \
+
+#que 25
+student_name = "Rahul"
+student_roll_number = "12"
+student_branch = "sci"
+
+#que 26
+marks = 75
+print("Before reassignment, marks =", marks)
+
+marks = 92
+print("After reassignment, marks =", marks)
+
+#que 27
+name, age, city = "Rahul", 18, "Patna"
+
+print(name)
+print(age)
+print(city)
+
+#que 28
+x = y = z = 0
+
+print(x, y, z)
+
+#que 29
+student1 = "Rahul"
+student_name = "Rahul"
+student_class = "B.Tech"
+
+print(student1)
+print(student_name)
+print(student_class)
+
+#que30
+student_name = "Aman"
+student_age = 17
+student_age = 18
+
+print(f"Name: {student_name}")
+print(f"Age: {student_age}")
