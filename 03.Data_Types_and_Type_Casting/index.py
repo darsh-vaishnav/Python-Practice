f0e085cce7229be22281b88042811d3d95e69317
Darsh <darsh@example.com>
2026-08-24 22:36:33 +0530

    data-type assign completed

---
+++ 03.Data_Types_and_Type_Casting/index.py
@@ -0,0 +1,84 @@
+# class work
+age=18
+Age=19.0
+aGe="20"
+AGe=True
+AgE=None
+
+
+print(type(age))
+print(type(Age))
+print(type(aGe))
+print(type(AGe))
+print(type(AgE))
+
+#que 24
+ 
+student_name="Rahul"
+student_age=18
+student_height=5.6
+whether_person_is_a_student=True
+student_result=None
+
+print(type(student_name))
+print(type(student_age))
+print(type(student_height))
+print(type(whether_person_is_a_student))
+print(type(student_result))
+
+# que 25
+
+a=50
+b=50.0
+c="50"
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#que 26
+
+a=True
+b="True"
+
+print(type(a))
+print(type(b))
+
+#que 27
+
+a=None
+b="None"
+
+print(type(a))
+print(type(b))
+
+#que 28
+
+value_int=19
+print(type(value_int))
+value=str(value_int)
+print(type(value))
+
+#que 29
+
+product_name="Fruit"
+product_quantity=20
+product_price="99.90"
+avability_of_product=True
+product_discount=None
+
+print(type(product_name))
+print(type(product_quantity))
+print(type(product_price))
+print(type(avability_of_product))
+print(type(product_discount))
+
+#que 30
+
+value_int=10
+value_float=10.0
+value_str="10"
+possibility_bol=True
+possibility_str="True"
+result_Nonetype=None
+result_str="None"
