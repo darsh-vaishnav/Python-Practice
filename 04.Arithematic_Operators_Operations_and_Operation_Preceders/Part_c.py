9512863fc9407ef95fbaea2b52d3c857421bab09
Darsh <darsh@example.com>
2026-08-26 08:51:30 +0530

    arithematic operator is completed

---
+++ 04.Arithematic_Operators_Operations_and_Operation_Preceders/Part_c.py
@@ -0,0 +1,237 @@
+# que 1
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#que 2
+a=10
+b=4.5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#que3
+subject_1=85
+subject_2=90
+subject_3=85
+
+total_marks=subject_1+subject_2+subject_3
+average_marks=total_marks/3
+
+print(total_marks)
+print(average_marks)
+
+#que 4
+product_price=100
+quantity=30
+
+total_price=product_price*quantity
+
+print(product_price*quantity)
+
+#que 5
+num=29
+reminder=num%2
+result=("Even","odd")[reminder]
+print(result)
+
+#que 6
+c=50
+d=20
+print(c/d)
+print(c//d)
+
+#que 7
+e=-19
+f=-11
+print(e+f)
+print(e-f)
+print(e*f)
+print(e/f)
+print(e//f)
+print(e%f)
+
+#que 8
+# Task 8 — Subtraction Edge Cases
+
+# 1. positive - positive
+a = 10
+b = 4
+print(a+b)
+
+# 2. positive - negative
+a = 10
+b = -4
+print(a+b)
+
+# 3. negative - positive
+a = -10
+b = 4
+print(a+b)
+
+# 4. negative - negative
+a = -10
+b = -4
+print(a+b)
+
+#que 9
+print(7//3)
+print(-7//3)
+print(7//-3)
+print(-7//-3)
+
+#que 10
+print(10%3)
+print(-10%3)
+print(10%-3)
+print(-10%-3)
+
+## Part 4
+
+#que 11
+
+print(10+5*2) #20
+print(20-4/2) #18.0
+print(10+20/5*2) #18.0
+print(2+3*4**2) #50
+print(100-20//5) #96
+
+#QUE 12
+print(10+5*2) #20
+print((10+5)*2) #30
+
+print(20-10/2) #15
+print((20-10)/2) #5
+
+print(2+3*4)#14
+print((2+3)*4)#20
+
+## Part 5
+
+print(True + True)
+print(True + False)
+
+print(True - False)
+print(False - True)
+
+print(True * True)
+print(True * False)
+
+print(True / True)
+print(False / True)
+
+print(True // True)
+
+print(True % True)
+
+print(True ** True)
+print(True ** False)
+print(False ** True)
+
+print(type(True + True))
+print(type(True / True))
+
+#que 14
+print(True+5)   # True is 1
+print(False+5)  # False is 0
+
+print(True*10)
+print(False*10)
+
+print(True-5)
+print(False-5)
+
+#que 15]
+
+
+first_name = "Darsh"
+last_name = "Vaishnav"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+#que16
+
+word = "Hello "
+print(word * 3)
+print(word * 3.5)
+
+#que 17
+
+print("Hello" + "World")
+print("Hello" - "World")
+print("Hello" * 3)
+print("Hello" / "World")
+
+#que 18
+value = None
+num = 10
+
+print(value + num)
+print(value - num)
+print(value * num)
+print(value / num)
+print(value // num)
+print(value % num)
+print(value ** num)
+
+#que19
+# Program 1
+a = 10
+b = 0
+result = a / b
+print(result)
+
+# Program 2
+str1 = "hello"
+str2 = "world"
+result = str1 - str2
+print(result)
+
+# Program 3
+x = None
+y = 10
+result = x + y
+print(result)
+
+# que 20
+
+a = 20
+b = 6
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
+
+#que 21
+a=10
+b=-3
+c=2.5
+
+print(a + b)  # 7
+print(a - c)  # 7.5
+print(a * b)  # -30
+print(a / c)  # 4.0
+print(a // b)  # -4
+print(a % b)  # -2
+print(a ** c)  # 316.22776601683796
+print((a + b) * c)  # 17.5
+print(a + b * c)  # 2.5
+print(a ** 2 + c / 2)  # 101.25
+print(-a + (b * c) // 2)  # -14.0
+print((a % c) ** 2 + b)  # -3.0
---
+++ 04.Arithematic_Operators_Operations_and_Operation_Preceders/Practice.py
@@ -0,0 +1,50 @@
+#que 1
+print(10 + 5) #15
+print(10 - 5) #5
+print(10 * 5) #50
+print(10 / 5) #2
+print(10 // 3) #3
+print(10 % 3) #1
+print(2 ** 4) #16
+#que 2
+print(10+5*2) #20
+print((10+5)*2) #30
+
+#que 3
+print(20/5*2) #8.0
+print(20//3*2)#12
+print(20-5+2) #17
+
+#que 4
+print(10-(-5))#15
+print(-10-5)#-15
+print(-10-(-5))#-5
+
+#que5
+print(10//3)#3
+print(-10//3)#-4
+print(10//-3)#-4
+print(-10//-3)#3
+
+#que 6
+print(10%3)#1
+print(-10%3)#2
+print(10%-3)#-2
+print(-10%-3)#-1
+
+#que 7
+print(-2**2)#-4
+print((-2)**2)#4
+print(-2**3)#-8
+print((-2)**3)#-8
+
+#que 8
+print(True+True)#2
+print(True+False)#1
+print(False*10)#0
+print(True*10)#1o
+print(True/2)#0.5
+
+#que 9
+print("Python"+"Programming")#PythonProgramming
+print("Python"*3)#PythonPythonPython
