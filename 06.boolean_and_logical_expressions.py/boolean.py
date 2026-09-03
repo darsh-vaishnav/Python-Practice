93714f08a9a1ef241d3ffa59cc0bf9a219c84473
Darsh <darsh@example.com>
2026-09-03 09:42:53 +0530

    Add boolean and logical expressions exercise

---
+++ 06.boolean_and_logical_expressions.py/boolean.py
@@ -0,0 +1,82 @@
+#Que 1
+
+print(10 + 5) #15(int)
+print(10 > 5) #True(bool)
+print("Python") #Python(str)
+print(10 == 20) #False(bool)
+
+#Que 2
+age = 20
+print(age >= 18)#True
+
+#Que 3
+marks = 45
+print(marks == 50)#False
+
+#Que 4
+age = 20
+print(age >= 18)#True
+
+#Que 5
+True and True    # True
+True and False   # False
+False and True   # False
+False and False  # False
+
+#Que 6
+True or True    # True
+True or False   # True
+False or True   # True
+False or False  # False
+
+#Que 7
+not True   # False
+not False  # True
+
+#Que 22
+age = 25
+
+print(age >= 18 and age <= 60) # True
+#print(age >= 18) True
+#print(age <= 60) True
+
+#Que 23
+age = 16
+
+print(age < 18 or age > 60) # True
+#print(age < 18) True
+#print(age > 60) False
+
+#Que 24
+
+age = 20
+
+print(not age < 18)# True
+#print(not age < 18) True
+
+#Que 25
+number = 25
+
+print(number > 10 and number < 50) # True
+
+#Que 26
+number = 150
+
+print(number < 10 or number > 100) # True
+
+#Que 27
+number = 5
+
+print(not number > 10) #True
+
+#Que 28
+print(bool(0))        # False
+print(bool(1))        # True
+print(bool(-5))       # True
+print(bool(""))       # False
+print(bool("Python")) # True
+print(bool(False))    # False
+print(bool(True))     # True
+print(bool(None))     # False
+
+#Que 29
