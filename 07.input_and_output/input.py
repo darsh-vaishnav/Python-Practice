3f148a4440a5c14197a4d199b9d2faf3ffda7807
Darsh <darsh@example.com>
2026-09-04 09:37:13 +0530

    i made the input and output assign

---
+++ 07.input_and_output/input.py
@@ -0,0 +1,40 @@
+# cw 
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+lab=input("Enter your lab: ")
+Product_name=input("Enter your Product name: ")
+Product_price=float(input("Enter your Product price: "))
+Product_quantity=int(input("Enter your Product quantity: "))
+Product_total=(Product_quantity)*(Product_price)
+
+print(f"Name {name}")
+print(f"Age {age}")
+print(f"Lab {lab}")
+print(f"Product Name {Product_name}")
+print(f"Product Price {Product_price}")
+print(f"Product Quantity {Product_quantity}")
+print(f"Product Total {Product_total}")
+
+print(type(name))
+print(type(age))
+print(type(lab))
+print(type(Product_name))
+print(type(Product_price))
+print(type(Product_quantity))
+print(type(Product_total))
+
+#Que 1
+name=input("Enter your name: ")
+print(f"Your name is {name}")
+
+
+#Que 2
+city=input("Enter your city: ")
+print(f"Your city is {city}")
+
+#Que 3
+user_name=input("Enter your Name:")
+age=int(input("Enter your Age:"))
+print(f"User name is {user_name} and your age is {age}")
+
+#Que 4
